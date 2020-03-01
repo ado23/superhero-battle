4f738debbd3910847243419c0a0c1d89dfa5cde4
ado23 <28600628+ado23@users.noreply.github.com>
2020-03-01 13:19:17 +0100

    Added styling of heroCard.

--- src/components/heroCard/index.js
+++ src/components/heroCard/index.js
@@ -1,12 +1,23 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./styles.scss";
 
 class HeroCard extends Component {
   render() {
     let { heroData } = this.props;
-    let { srcImg } = this.props;
 
-    console.log("HERO DATA: ", heroData);
+    let srcImg = heroData[0].images.sm;
+    let { name } = heroData[0];
+    let {
+      fullName,
+      placeOfBirth,
+      aliases,
+      publisher,
+      alignment,
+      firstAppearance
+    } = heroData[0].biography;
+
+    let work = heroData[0].work.occupation;
+    let connections = heroData[0].connections.groupAffiliation;
 
     return (
       <div className="container">
@@ -16,12 +27,73 @@ class HeroCard extends Component {
           </div>
 
           <div className="content">
-            <h3 className="title">BLACK KNIGHT (SIR PERCY)</h3>
+            <h3 className="title">{name}</h3>
             <div className="description">
-              <p>
-                Sir Percy of Scandia, also known as the original Black Knight,
-                is a fictional character in the Marvel Universe.
-              </p>
+              <p>Full name: {fullName}</p>
+              <p> Place of birth: {placeOfBirth}</p>
+              <p>Aliases: {aliases}</p>
+              <p> Publisher: {publisher}</p>
+              <p> Aligment: {alignment} </p>
+              <p>First appearance: {firstAppearance} </p>
+              <p> Occupation: {work} </p>
+              <p>Connctions: {connections}</p>
+            </div>
+          </div>
+        </article>
+        <article className="article">
+          <div className="thumbnail">
+            <img className="thumbnail_image" src={srcImg} alt="sss" />
+          </div>
+
+          <div className="content">
+            <h3 className="title">{name}</h3>
+            <div className="description">
+              <p>Full name: {fullName}</p>
+              <p> Place of birth: {placeOfBirth}</p>
+              <p>Aliases: {aliases}</p>
+              <p> Publisher: {publisher}</p>
+              <p> Aligment: {alignment} </p>
+              <p>First appearance: {firstAppearance} </p>
+              <p> Occupation: {work} </p>
+              <p>Connctions: {connections}</p>
+            </div>
+          </div>
+        </article>
+        <article className="article">
+          <div className="thumbnail">
+            <img className="thumbnail_image" src={srcImg} alt="sss" />
+          </div>
+
+          <div className="content">
+            <h3 className="title">{name}</h3>
+            <div className="description">
+              <p>Full name: {fullName}</p>
+              <p> Place of birth: {placeOfBirth}</p>
+              <p>Aliases: {aliases}</p>
+              <p>First appearance: {firstAppearance} </p>
+              <p> Publisher: {publisher}</p>
+              <p> Aligment: {alignment} </p>
+              <p> Occupation: {work} </p>
+              <p>Connctions: {connections}</p>
+            </div>
+          </div>
+        </article>
+        <article className="article">
+          <div className="thumbnail">
+            <img className="thumbnail_image" src={srcImg} alt="sss" />
+          </div>
+
+          <div className="content">
+            <h3 className="title">{name}</h3>
+            <div className="description">
+              <p>Full name: {fullName}</p>
+              <p> Place of birth: {placeOfBirth}</p>
+              <p>Aliases: {aliases}</p>
+              <p>First appearance: {firstAppearance} </p>
+              <p> Publisher: {publisher}</p>
+              <p> Aligment: {alignment} </p>
+              <p> Occupation: {work} </p>
+              <p>Connctions: {connections}</p>
             </div>
           </div>
         </article>
@@ -31,7 +103,27 @@ class HeroCard extends Component {
 }
 
 HeroCard.defaultProps = {
-  srcImg: "https://community.algolia.com/marvel-search/img/hit-default.jpg"
+  heroData: {
+    name: "Unknown",
+    powerstats: {
+      intelligence: 0,
+      strength: 0,
+      speed: 0,
+      durability: 0,
+      power: 0,
+      combat: 0
+    },
+    appearance: {
+      height: ["1", "1 cm"],
+      weight: ["1", "1 kg"]
+    },
+    images: {
+      md: "https://community.algolia.com/marvel-search/img/hit-default.jpg"
+    },
+    biography: {
+      publisher: "unknown"
+    }
+  }
 };
 
 export default HeroCard;
